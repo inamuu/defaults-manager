62ce42ab31d86360bf8febb1fd3df873b141254d
inamuu <8310973+inamuu@users.noreply.github.com>
2022-08-21 14:36:11 +0900

    Add a sub command that read

---
+++ cmd/read.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+// readCmd represents the read command
+var readCmd = &cobra.Command{
+	Use:   "read",
+	Short: "Print defaults settings",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("read called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(readCmd)
+	fmt.Println("Run: write command")
+	out, _ := exec.Command("ls", "-la").Output()
+	fmt.Printf("result: \n%s", out)
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// readCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// readCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
